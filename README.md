79472f9a348a707a2aefcecdc37df8f705c13c04
Sreelakshmi-Odatt <sreelakshmiodatt@gmail.com>
2023-12-20 12:59:17 -0500

    Calculator
    A simple calculator for basic arithmetic operations like addition , subtraction , division and multiplication.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Python-Calculator
-A simple calculator using python
+# Simple Calculator Using Python
+
+
+## Author
+
+- Sreelakshmi Odatt Venu
+
+## Date Created
+
+- 20/12/2023
+
+## Description
+
+This Python script is a simple calculator that performs basic arithmetic operations such as addition, subtraction, multiplication, and division. It takes two numerical inputs and an operator from the user to perform the calculation.
+
+## Usage
+
+1. Run the script in a Python environment.
+2. Enter the first numerical value when prompted.
+3. Enter a valid operator (`+`, `-`, `*`, `/`) when prompted.
+4. Enter the second numerical value when prompted.
+5. The script will calculate and display the result.
+
+## Input Validation
+
+- The script handles input validation for numerical values, ensuring that the user enters a valid number.
+- The script also checks for a valid operator, allowing only `+`, `-`, `*`, and `/`.
+
+## Error Handling
+
+- The script uses try-except blocks to handle potential errors, such as division by zero or invalid input.
+- If the user provides invalid input or an unexpected error occurs, a generic error message is displayed.
+
---
+++ calculator.py
@@ -0,0 +1,62 @@
+# Author: Sreelakshmi Odatt Venu
+# Date Created: 20/12/2023
+
+# Function to add two numbers
+def add_number(a, b):
+    return a + b
+
+# Function to subtract two numbers
+def subtract_number(a, b):
+    return a - b
+
+# Function to multiply two numbers
+def multiply_number(a, b):
+    return a * b
+
+# Function to divide numbers, handling division by zero
+def divide_number(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
+
+# Get user input for the first number , check if the user input the valid first digit and handle the exception
+while True:
+    try:
+        number_1 = float(input("Enter the first number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+# Get user input for the operator , check if the user input the valid operator and if not , handle the exception
+while True:
+    operator = input("Enter the operator (+, -, *, /): ")
+    if operator in ('+', '-', '*', '/'):
+        break
+    else:
+        print("Please enter a valid operator.")
+
+# Get user input for the second number check if the user input the valid second number and if not , handle the exception
+while True:
+    try:
+        number_2 = float(input("Enter the second number: "))
+        break
+    except ValueError:
+        print("Please enter a valid number.")
+
+# Perform calculation based on the operator and user input 
+if operator == '+':
+    result = add_number(number_1, number_2)
+elif operator == '-':
+    result = subtract_number(number_1, number_2)
+elif operator == '*':
+    result = multiply_number(number_1, number_2)
+elif operator == '/':
+    result = divide_number(number_1, number_2)
+else:
+    result = "Error Occured ! "
+
+# Display the calculated result 
+
+print(f"Result: {result}")
